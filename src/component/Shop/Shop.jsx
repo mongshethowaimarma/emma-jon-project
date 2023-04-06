d63a64e223d4eb4b06904fb2e1d1c039d76135f4
mongshethoiwmarma1 <mongshethowaimarma.pstu@gmail.com>
2023-04-06 15:19:15 +0600

    added shop cart with condition

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import Product from '../Product/Product';
 import Cart from '../../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
@@ -10,10 +11,25 @@ const Shop = () => {
         .then(res=> res.json())
         .then(data=> setProducts(data))
     }, []);
-
+    useEffect(()=>{
+        const storedCart = getShoppingCart();
+        const saveCart = [];
+        for (const id in storedCart){
+            const addedProduct = products.find(product=> product.id ===id)
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                saveCart.push(addedProduct);  
+            }
+            
+            setCart(saveCart)
+            
+        }
+    }, [products])
     const handleAddToCart = (product)=>{
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
 
     return (
