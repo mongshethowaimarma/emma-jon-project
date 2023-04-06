0dfbaf57cc002b399ca9f430eead63116b2874b3
mongshethoiwmarma1 <mongshethowaimarma.pstu@gmail.com>
2023-04-06 10:09:35 +0600

    added total price and grandtotal

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -3,19 +3,23 @@ import './Cart.css'
 const Cart = ({cart}) => {
     // const cart = props.cart;
     // const {cart} = props;
-    let total = 0;
+    let totalPrice = 0;
+    let totalShippingPrice =0;
     for (const product of cart){
-        total = total + product.price;
+        totalPrice = totalPrice + product.price;
+        totalShippingPrice = totalShippingPrice + product.shipping;
     }
+    const tax = totalPrice*7/100;
+    const grandTotal = totalPrice+ totalShippingPrice+ tax;
 
     return (
         <div className='cart'>
             <h4>Order summary</h4>
             <p>Selected Items: {cart.length}</p>
-            <p>Total price: {total}</p>
-            <p>Total shipping:</p>
-            <p>Tax:</p>
-            <h6>Grand total:</h6>
+            <p>Total price: {totalPrice}</p>
+            <p>Total shipping:{totalShippingPrice}</p>
+            <p>tax: {tax.toFixed(2)}</p>
+            <h6>Grand total: {grandTotal.toFixed(2)}</h6>
         </div>
     );
 };
