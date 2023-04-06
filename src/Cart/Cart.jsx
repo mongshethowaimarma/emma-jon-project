87b86d8125e3ae5b245a8a3368df0bdea8ba0e9a
mongshethoiwmarma1 <mongshethowaimarma.pstu@gmail.com>
2023-04-06 16:08:14 +0600

    finnally added shopping cart

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -5,9 +5,15 @@ const Cart = ({cart}) => {
     // const {cart} = props;
     let totalPrice = 0;
     let totalShippingPrice =0;
+    let quantity= 0;
     for (const product of cart){
-        totalPrice = totalPrice + product.price;
+        // if(product.quantity === 0){
+        //     product.quantity =1;
+        // }
+        // // product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price + product.quantity;
         totalShippingPrice = totalShippingPrice + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice*7/100;
     const grandTotal = totalPrice+ totalShippingPrice+ tax;
@@ -15,10 +21,10 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h4>Order summary</h4>
-            <p>Selected Items: {cart.length}</p>
-            <p>Total price: {totalPrice}</p>
-            <p>Total shipping:{totalShippingPrice}</p>
-            <p>tax: {tax.toFixed(2)}</p>
+            <p>Selected Items: {quantity}</p>
+            <p>Total price: ${totalPrice}</p>
+            <p>Total shipping:${totalShippingPrice}</p>
+            <p>tax: ${tax.toFixed(2)}</p>
             <h6>Grand total: {grandTotal.toFixed(2)}</h6>
         </div>
     );
